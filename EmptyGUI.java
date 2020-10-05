b66b6d4d8c1af7bbbba172e888d0096c08bd63a4
Chin <chinxdestiny@gmail.com>
2020-10-05 18:18:51 +0800

    Draft an GUI Sample

---
+++ EmptyGUI.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class EmptyGUI {
+    
+    public static void Page(){
+        JFrame      screen = new JFrame();
+
+        screen.setTitle("OXGame");
+        screen.setVisible(true);
+        screen.setSize(1024,780);
+        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        screen.validate();
+
+    }
+
+    public static void main(String args[]){
+        Page();
+    }
+}
