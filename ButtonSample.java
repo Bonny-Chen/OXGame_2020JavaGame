d17ea5a69f06315e532b5e0387645013e4b9311f
Bonny-Chen <bonny1989dae@gmail.com>
2020-10-28 23:17:20 +0800

    homepage done

--- ButtonSample.java
+++ ButtonSample.java
@@ -13,33 +13,33 @@ public class ButtonSample {
         Image       image;
         Image       tmpimg;
 
-        //
+        // Default setting
         screen.setTitle("ButtonSample");
         screen.setVisible(true);
         screen.setSize(1024,780);
         screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //
+        // Set image position
         imageIcon = new ImageIcon("Img/info.png");
         image = imageIcon.getImage();
         tmpimg = image.getScaledInstance(200, 120,  java.awt.Image.SCALE_SMOOTH);
         imageIcon = new ImageIcon(tmpimg);
 
 
-        // 
+        // Add button
         JButton OKBtn = new JButton("OK");
         jp.add(OKBtn);
         OKBtn.setPreferredSize(new Dimension(100,50));   // Resize Btn (w,h)
         
-        //
+        //Add image to button
         imageIcon = new ImageIcon("Img\\info.png");
         JButton ImgBtn = new JButton(imageIcon);
-        ImgBtn.setContentAreaFilled(false);             //
-        ImgBtn.setFocusable(false);                     
-        ImgBtn.setBorderPainted(false);                 //
+        ImgBtn.setContentAreaFilled(false);             // Remove Btn Background
+        ImgBtn.setFocusable(false);                     //inside line invisible
+        ImgBtn.setBorderPainted(false);                 //outside line invisible
         jp.add(ImgBtn);
 
-        //
+        // Button mouse over background setting
         ImgBtn.addMouseListener(new java.awt.event.MouseAdapter(){
             public void mouseEntered(java.awt.event.MouseEvent evt) {
                 ImgBtn.setContentAreaFilled(true); 
@@ -52,7 +52,7 @@ public class ButtonSample {
             }
         });
         
-        //
+        // Click count
         ImgBtn.addActionListener(new ActionListener() {
             int         count = 0;
             @Override
--- Homepage.java
+++ Homepage.java
@@ -1,3 +1,8 @@
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
 import java.awt.*;
 import javax.swing.*;
 
@@ -10,13 +15,9 @@ public class Homepage {
         JButton     HowToBtn = new JButton("HOW TO");
         JButton     ExitBtn = new JButton("EXIT");
         JLabel      titLabel = new JLabel("OX Game");
-        ImageIcon   icon = new ImageIcon("Img\\Tic-Tag-Toe.png");
-        Image       imageIcon;
-        Image       tmpimg;
-        ImageIcon       HowtoImg;
+        ImageIcon   logoIcon = new ImageIcon("Img\\logo.png");
         JLabel      ImgLabel = new JLabel();
-        JLabel      jl = new JLabel();
-        JLabel      TEST = new JLabel();            // TEST FOR PUSH
+        
 
 
         // Default Setting
@@ -27,11 +28,9 @@ public class Homepage {
         jp.setLayout(null);     //for position
 
         // Show image
-        imageIcon = icon.getImage();
-        tmpimg = imageIcon.getScaledInstance(300, 300, java.awt.Image.SCALE_SMOOTH);
-        icon = new ImageIcon(tmpimg);
-        ImgLabel.setBounds(290,190,300,300);            //Image setting(x,y,width,heigh)
-        ImgLabel.setIcon(icon);
+        logoIcon.setImage(logoIcon.getImage().getScaledInstance(200,120,Image.SCALE_DEFAULT));
+        ImgLabel.setBounds(300,190,300,300);            //Image setting(x,y,width,heigh)
+        ImgLabel.setIcon(logoIcon);
         jp.add(ImgLabel);
 
         // titLabel Setting (Title)
@@ -41,22 +40,45 @@ public class Homepage {
         jp.add(titLabel);
 
         // Background setting
-        jp.setBackground(Color.decode("#123456"));
+        jp.setBackground(Color.decode("#888CCCA"));
+
+        // HowTo button change
+        HowToBtn.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //System.out.println("Btn Click");
+                HowToBtn.setContentAreaFilled(false);   // Remove Btn Background
+                HowToBtn.setFocusable(false);           //inside line invisible
+                HowToBtn.setBorderPainted(false);       //outside line invisible
+            }
+        });
 
         // button setting
+        StartBtn.setFocusable(false);                           //inside line invisible               
+        StartBtn.setBorderPainted(false);                       //outside line invisible
+        HowToBtn.setFocusable(false);                                         
+        HowToBtn.setBorderPainted(false);                      
+        ExitBtn.setFocusable(false);                                       
+        ExitBtn.setBorderPainted(false);                       
+
         StartBtn.setBounds(630, 300, 160, 30);                  //set position
         HowToBtn.setBounds(630,350,160,30);
         ExitBtn.setBounds(630, 400, 160, 30);
-        StartBtn.setBackground(Color.decode("#1CBACDE"));        //set background color
-        HowToBtn.setBackground(Color.decode("#1CBACDE"));
-        ExitBtn.setBackground(Color.decode("#1CBACDE"));
-        StartBtn.setForeground(Color.decode("#123AAA"));        //set text color
-        HowToBtn.setForeground(Color.decode("#123AAA"));
-        ExitBtn.setForeground(Color.decode("#123AAA"));
+        StartBtn.setBackground(Color.decode("#EEEABA"));        //set background color
+        HowToBtn.setBackground(Color.decode("#EEEABA"));
+        ExitBtn.setBackground(Color.decode("#EEEABA"));
+        StartBtn.setForeground(Color.decode("#FF12345"));        //set text color
+        HowToBtn.setForeground(Color.decode("#FF12345"));
+        ExitBtn.setForeground(Color.decode("#FF12345"));
+
+        
+         
 
         jp.add(StartBtn);
         jp.add(HowToBtn);
         jp.add(ExitBtn);
+        
 
         screen.add(jp);
         screen.validate();
