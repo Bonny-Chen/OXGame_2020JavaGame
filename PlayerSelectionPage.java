e86530c5fa8d5c56ebcac84030a981532bb37330
Bonny-Chen <bonny1989dae@gmail.com>
2020-11-15 23:45:58 +0800

    OXGamepage in playerpage

--- PlayerSelectionPage.java
+++ PlayerSelectionPage.java
@@ -7,9 +7,8 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class PlayerSelectionPage extends JFrame {
-    private JPanel jp, HomePanel;
+    private JPanel jp;
     private PlayerSelectionPage screen;
-
     public static void main(String[] args) {
         PlayerSelectionPage GUI = new PlayerSelectionPage();
         GUI.Init();
@@ -32,7 +31,7 @@ public class PlayerSelectionPage extends JFrame {
 
     public void PlayerSelectionPage() {
         jp = new JPanel();
-        jp.setBackground(Color.decode("#000339"));
+        jp.setBackground(Color.decode("#299666"));
         jp.setLayout(null);
         Button = new ButtonImageCreate[6];
         for (int x = 0; x < Button.length; x++) {
@@ -56,11 +55,127 @@ public class PlayerSelectionPage extends JFrame {
     }
 
     public void HomePage() {
-        HomePanel = new JPanel();
-        JLabel Label;
-        Label = new JLabel("HomePage");
-        HomePanel.add(Label);
-        screen.add(HomePanel);
+        
+        jp = new JPanel();
+        JButton     StartBtn = new JButton("START");
+        JButton     HowToBtn = new JButton("HOW TO");
+        JButton     ExitBtn = new JButton("EXIT");
+        JLabel      titLabel = new JLabel("OX Game");
+        ImageIcon   logoIcon = new ImageIcon("Img\\logo.png");
+        JLabel      ImgLabel = new JLabel();
+
+        // Default Setting
+        screen.setTitle("Home Page");
+        screen.setVisible(true);
+        screen.setSize(900, 630);
+        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jp.setLayout(null);     //for position
+
+        // Show image
+        logoIcon.setImage(logoIcon.getImage().getScaledInstance(350,330,Image.SCALE_DEFAULT));
+        ImgLabel.setBounds(280,100,500,500);            //Image setting(x,y,width,heigh)
+        ImgLabel.setIcon(logoIcon);
+        jp.add(ImgLabel);
+
+        // titLabel Setting (Title)
+        titLabel.setBounds(350, 100, 300, 50);                   // Title setting (x,y,width,heigh)
+        titLabel.setFont(new java.awt.Font("Dialog", 4, 50));   // Resize Font
+        titLabel.setForeground(Color.decode("#FFFFFF")); 
+        jp.add(titLabel);
+
+        // Background setting
+        jp.setBackground(Color.decode("#888CCCA"));
+
+        // HowTo button change
+        HowToBtn.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //System.out.println("Btn Click");
+                HowToBtn.setContentAreaFilled(false);   // Remove Btn Background
+                HowToBtn.setFocusable(false);           //inside line invisible
+                HowToBtn.setBorderPainted(false);       //outside line invisible
+            }
+        });
+        StartBtn.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //System.out.println("Btn Click");
+                screen.remove(jp);                                  // Clear the screen
+                screen.setTitle("Player Select");
+                PlayerSelectionPage();
+                screen.validate();
+            }
+        });
+        
+        // button setting
+        StartBtn.setFocusable(false);                           //inside line invisible               
+        StartBtn.setBorderPainted(false);                       //outside line invisible
+        HowToBtn.setFocusable(false);                                         
+        HowToBtn.setBorderPainted(false);                      
+        ExitBtn.setFocusable(false);                                       
+        ExitBtn.setBorderPainted(false);                       
+
+        StartBtn.setBounds(630, 300, 160, 30);                  //set position
+        HowToBtn.setBounds(630,350,160,30);
+        ExitBtn.setBounds(630, 400, 160, 30);
+        StartBtn.setBackground(Color.decode("#EEEABA"));        //set background color
+        HowToBtn.setBackground(Color.decode("#EEEABA"));
+        ExitBtn.setBackground(Color.decode("#EEEABA"));
+        StartBtn.setForeground(Color.decode("#FF12345"));        //set text color
+        HowToBtn.setForeground(Color.decode("#FF12345"));
+        ExitBtn.setForeground(Color.decode("#FF12345"));
+
+        jp.add(StartBtn);
+        jp.add(HowToBtn);
+        jp.add(ExitBtn);
+
+        screen.add(jp);
+        screen.validate();
+        // HomePanel.add(Label);
+        // screen.add(HomePanel);
+    }
+    ImageIcon player1;
+    JLabel    player1Lab = new JLabel();
+    int round =0;
+    
+    public void OXGamePage(){
+        jp = new JPanel();
+        ImageIcon   oxIcon = new ImageIcon("Img\\ox.png");
+        JLabel      ImgLabel = new JLabel();
+
+        // Default Setting
+        screen.setTitle("OXGame");
+        screen.setVisible(true);
+        screen.setSize(900, 630);
+        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jp.setLayout(null);     //for position
+        //
+        Button = new ButtonImageCreate[9];
+        for (int x = 0; x < Button.length; x++) {
+            Button[x] = new ButtonImageCreate(x, "Img/o.png",
+                    "Img/Player1-" + Integer.toString(x + 1) + ".png", (x + 1) * (x + 200) + (x - 50), 100, 170, 230);
+            Button[x].addActionListener(BH);
+            Button[x].addMouseListener(MH);
+            
+        }
+        // player1.setBounds(0,0,0,0);
+        jp.add(player1Lab);
+        // Show ox image
+        oxIcon.setImage(oxIcon.getImage().getScaledInstance(460,440,Image.SCALE_DEFAULT));
+        ImgLabel.setBounds(220,50,600,500);            //Image setting(x,y,width,heigh)
+        ImgLabel.setIcon(oxIcon);
+        jp.add(ImgLabel);
+
+        // player1.setImage(player1.getImage().getScaledInstance(460,440,Image.SCALE_DEFAULT));
+        // ImgLabel.setBounds(220,50,600,500);            //Image setting(x,y,width,heigh)
+        // ImgLabel.setIcon(player1);
+        // jp.add(ImgLabel);
+       
+
+        screen.add(jp);
+        screen.validate();
     }
 
     private class ButtonHandler implements ActionListener {
@@ -70,6 +185,7 @@ public class PlayerSelectionPage extends JFrame {
         public void actionPerformed(ActionEvent e) {
             ButtonImageCreate myBtn = (ButtonImageCreate) e.getSource();
             ButtonImageCreate tmpBtn = myBtn;
+            // ButtonImageCreate sendBtn = tmpBtn;
             if (ExistsLastClick() >= 0 && ExistsLastClick() != 4) {
 
                 System.out.println("LastClick " + ExistsLastClick());
@@ -82,6 +198,19 @@ public class PlayerSelectionPage extends JFrame {
             if (myBtn.getID() == 5) {                               
                 screen.remove(jp);                                  // Clear the screen
                 HomePage();
+                screen.validate();
+            }
+
+            //Switch to OXGamePage
+            if (myBtn.getID() == 4) {                               
+                screen.remove(jp); 
+                int x = tmpBtn.getID();
+                player1 = new ImageIcon("Img/Player1-" + Integer.toString(x + 1) + ".png");
+                player1.setImage(player1.getImage().getScaledInstance(180,250,Image.SCALE_DEFAULT));
+                player1Lab.setBounds(30,70,300,400);            //Image setting(x,y,width,heigh)
+                player1Lab.setIcon(player1);
+                // player1.setIcon(tmpBtn.iconHover);
+                OXGamePage();
                 screen.validate();
             }
             myBtn.setIcon(myBtn.iconHover);
@@ -162,5 +291,6 @@ class ButtonImageCreate extends JButton {
     public int IsClicked() {
         return Clicked;
     }
+    
 
 }
